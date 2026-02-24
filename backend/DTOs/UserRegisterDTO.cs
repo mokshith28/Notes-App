a919b525e38efb69c0a86a34d729620a77be7a5a
Mokshith <mokshithban2004@gmail.com>
2026-02-24 10:59:33 +0530

    add auth controller

---
+++ backend/DTOs/UserRegisterDTO.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace backend.DTOs
+{
+    public class UserRegisterDTO
+    {
+        [Required]
+        public string Username { get; set; } = string.Empty;
+
+        [Required, EmailAddress]
+        public string Email { get; set; } = string.Empty;
+
+        [Required, MinLength(8)]
+        public string Password { get; set; } = string.Empty;
+    }
+}
